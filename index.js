33b97e9a86138d86eb19e74d84a9c192e3ce66e9
Anton Lazukin <kevin6121991@gmail.com>
2025-01-01 18:57:00 +0300

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+}
+
+class D {};
+
+
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+}
