90f29027877e743b3f8245dfb34b2d9e525ad351
Anton Lazukin <kevin6121991@gmail.com>
2025-01-12 16:58:49 +0300

    all files are copied-1

--- index.js
+++
@@ -1,9 +0,0 @@
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-}
-
-class D {}
-
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-}
